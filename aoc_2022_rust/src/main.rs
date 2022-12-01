f7f614c7f21ee74d2deb931823bf0fb59e4def1a
Andre Bossard <anbossar@microsoft.com>
2022-12-01 23:50:49 +0100

    refactoring

--- aoc_2022_rust/src/main.rs
+++ aoc_2022_rust/src/main.rs
@@ -1,10 +1,9 @@
 use std::fs;
 mod day_01;
-
 fn main() {
-    let input = fs::read_to_string("./src/day_01/input.txt").expect("Error reading file");
-    let day_1_result = crate::day_01::dwarf_calories::day_01(&input);    
+    let input = fs::read_to_string("./src/day_01_input.txt").expect("Error reading file");
+    let day_1_result = day_01::day_01_a(&input);    
     println!("Day 1 result: {}", day_1_result);   
-    let day_1_b_result = crate::day_01::dwarf_calories::day_01_b(&input);    
-    println!("Day 1b result: {}", day_1_b_result);   
+    let day_1_b_result = day_01::day_01_b(&input);    
+    println!("Day 1b result: {}", day_1_b_result);
 }
