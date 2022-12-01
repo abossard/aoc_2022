047f70e1f506ed61be320e4e27da8948b66955b4
Andre Bossard <anbossar@microsoft.com>
2022-12-02 00:27:32 +0100

    yes

--- aoc_2022_rust/src/day_01.rs
+++ aoc_2022_rust/src/day_01.rs
@@ -2,17 +2,17 @@
 /// 1. for each line
 /// 2. parse to u32 and force unwrap (not nice, but good enough)
 /// 3. sum up
-fn dwarf_sum(input: &str)-> u32 {
+fn dwarf_sum(input: &str) -> u32 {
     input.lines().map(|line| line.parse::<u32>().unwrap()).sum()
 }
 
 /// splits the input on empty lines (double newlines) and then calls dwarf_sum on each part
 fn dwarf_creater(input: &str) -> Vec<u32> {
-    let mut dwarfs:Vec<u32> = input.split("\n\n").map(dwarf_sum).collect();
+    let mut dwarfs: Vec<u32> = input.split("\n\n").map(dwarf_sum).collect();
     dwarfs.sort();
     return dwarfs;
 }
-    
+
 pub fn day_01_a(input: &str) -> u32 {
     let dwarfs = dwarf_creater(input);
     return dwarfs[dwarfs.len() - 1];
@@ -20,5 +20,5 @@ pub fn day_01_a(input: &str) -> u32 {
 
 pub fn day_01_b(input: &str) -> u32 {
     let dwarfs = dwarf_creater(input);
-    return dwarfs.as_slice()[dwarfs.len() -3..].iter().sum();
+    return dwarfs.as_slice()[dwarfs.len() - 3..].iter().sum();
 }
--- aoc_2022_rust/src/main.rs
+++ aoc_2022_rust/src/main.rs
@@ -5,15 +5,14 @@ fn get_input_day(number: u8) -> String {
     fs::read_to_string(format!("./src/day_{:02}_input.txt", number)).expect("Error reading file")
 }
 
-
 fn run_fn_day<R>(number: u8, func: fn(&str) -> R) -> R {
     func(&get_input_day(number))
 }
 
 fn main() {
-    let day_1_result = run_fn_day(1, day_01::day_01_a);    
-    println!("Day 1 result: {}", day_1_result);   
-    let day_1_b_result = run_fn_day(1, day_01::day_01_b);   
+    let day_1_result = run_fn_day(1, day_01::day_01_a);
+    println!("Day 1 result: {}", day_1_result);
+    let day_1_b_result = run_fn_day(1, day_01::day_01_b);
     println!("Day 1b result: {}", day_1_b_result);
 }
 
