79f6862eb53a6367b0a028659f9f8646d6927ac1
Andre Bossard <anbossar@microsoft.com>
2022-12-02 00:08:11 +0100

    tests

--- aoc_2022_rust/src/day_01.rs
+++ aoc_2022_rust/src/day_01.rs
@@ -1,7 +1,12 @@
+/// sums up a string of lines with some empty lines in between
+/// 1. for each line
+/// 2. parse to u32 and force unwrap (not nice, but good enough)
+/// 3. sum up
 fn dwarf_sum(input: &str)-> u32 {
     input.lines().map(|line| line.parse::<u32>().unwrap()).sum()
 }
 
+/// splits the input on empty lines (double newlines) and then calls dwarf_sum on each part
 fn dwarf_creater(input: &str) -> Vec<u32> {
     let mut dwarfs:Vec<u32> = input.split("\n\n").map(dwarf_sum).collect();
     dwarfs.sort();
